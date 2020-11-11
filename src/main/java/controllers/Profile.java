d38a2b24f6807e6c1ed76d00586cb342a25ee590
AgakhanPeyishbek <57942020+AgakhanPeyishbek@users.noreply.github.com>
2020-11-11 20:39:27 +0600

    Fully working Authentication

--- src/main/java/controllers/Profile.java
+++ src/main/java/controllers/Profile.java
@@ -1,10 +1,14 @@
 package controllers;
 
+import models.User;
 import security.implementation.AuthorityProviderImpl;
 import security.interfaces.AuthorityProvider;
+import services.implementations.UserServiceImpl;
+import services.interfaces.UserService;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,14 +17,38 @@ import java.io.IOException;
 @WebServlet(name = "Profile")
 public class Profile extends HttpServlet {
     private final AuthorityProvider authorityProvider = new AuthorityProviderImpl();
+    private final UserService userService=new UserServiceImpl();
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        authorityProvider.isAuthenticated(request, response);
-
+        if(authorityProvider.isAuthenticated(request, response)){
+            Cookie[] cookies =request.getCookies();
+            //Displaying User name value from cookie
+            for (Cookie cookie:cookies){
+                if(cookie.getName().equals("username")){
+                    cookie.setMaxAge(0);
+                    cookie.setValue("");
+                    response.addCookie(cookie);
+                }
+                if(cookie.getName().equals("role")){
+                    cookie.setMaxAge(0);
+                    cookie.setValue("");
+                    response.addCookie(cookie);
+                }
+            }
+        }
+        response.sendRedirect(request.getContextPath()+"/main");
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         if(authorityProvider.isAuthenticated(request, response)){
+            User user=null;
+            Cookie[] cookies=request.getCookies();
+            for(Cookie cookie:cookies){
+                if(cookie.getName().equals("username")){
+                    user=userService.getUserByUsername(cookie.getValue());
+                }
+            }
+            request.setAttribute("user",user);
             request.getRequestDispatcher("/example.jsp").forward(request, response);
         }else {
             response.sendRedirect(request.getContextPath()+"/main");
