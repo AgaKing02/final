0982c0f406dd1a42d0f11720241dbd5822e53dbc
AgakhanPeyishbek <57942020+AgakhanPeyishbek@users.noreply.github.com>
2020-11-11 14:06:34 +0600

    JSP pages for @GET adminstration

--- src/main/java/kz/team/se1908/repositories/implementations/database/RepositoryImpl.java
+++ src/main/java/kz/team/se1908/repositories/implementations/database/RepositoryImpl.java
@@ -2,11 +2,22 @@ package kz.team.se1908.repositories.implementations.database;
 
 import kz.team.se1908.repositories.interfaces.Repository;
 
+import javax.ws.rs.ServerErrorException;
+import javax.ws.rs.core.Response;
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class RepositoryImpl implements Repository {
     @Override
     public Connection getConnection() {
-        return null;
+        try {
+            try {
+                Class.forName("org.postgresql.Driver");
+            } catch (ClassNotFoundException e) { e.printStackTrace(); }
+            return DriverManager.getConnection("jdbc:postgresql://localhost:5433/finaljavaproject", "postgres", "agahan02");
+        } catch (SQLException ex) {
+            throw new ServerErrorException("Cannot connect to DB: " + ex.getMessage(), Response.Status.INTERNAL_SERVER_ERROR);
+        }
     }
 }
