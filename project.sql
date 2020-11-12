ac8a38aa4ff23e8a5c1469cc7248d04a32f41a2a
AgakhanPeyishbek <57942020+AgakhanPeyishbek@users.noreply.github.com>
2020-11-12 13:16:09 +0600

    Fully working interaction  between Classes

--- project.sql
+++ project.sql
@@ -1,68 +1,73 @@
 CREATE TABLE users (
-    id int primary key,
-    username varchar(255),
-    name varchar(255),
-	surname varchar(255),
-    password varchar(255),
-    role varchar(255)
+                       id serial  primary key,
+                       username varchar(255),
+                       name varchar(255),
+                       surname varchar(255),
+                       password varchar(255),
+                       role varchar(255)
 );
 CREATE TABLE groups (
-    g_id int primary key,
-    g_name varchar(255),
-    g_year int 
+                        id serial primary key,
+                        name varchar(255),
+                        year int
 );
 CREATE TABLE clubs(
-    c_id int primary key,
-    c_name varchar(255),
-    e_description varchar(255)
+                      id serial primary key,
+                      name varchar(255),
+                      description varchar(255)
 );
 
 CREATE TABLE clubstudent (
-    cs_id int primary key,
-    club_id int,
-    volunteers_id int 
+                             id serial primary key,
+                             clubid int,
+                             studentid int
 );
 CREATE TABLE groupstudent (
-    gs_id int primary key,
-    group_id int,
-    student_id int 
+                              id serial primary key,
+                              groupid int,
+                              studentid int
 );
+
+
 CREATE TABLE news (
-    n_id int primary key,
-    n_title varchar(255),
-	n_description varchar(255),
-    publisher int 
+                      id int primary key,
+                      title varchar(255),
+                      description varchar(255),
+                      publisher int
 );
+
+
 CREATE TABLE events (
-    e_id int primary key,
-    e_event varchar(255),
-	e_description varchar(255)
+                        id serial primary key,
+                        event varchar(255),
+                        description varchar(255)
 );
 CREATE TABLE eventstudent(
-    e_id int primary key,
-    event_id int,
-    listener_id int
+                             id int primary key,
+                             eventid int,
+                             studentid int
 );
 ALTER TABLE groupstudent
-ADD FOREIGN KEY (student_id) REFERENCES users(id);
+    ADD FOREIGN KEY (studentid) REFERENCES users(id);
 
 ALTER TABLE groupstudent
-ADD FOREIGN KEY (group_id) REFERENCES groups(g_id);
+    ADD FOREIGN KEY (groupid) REFERENCES groups(id);
 
 ALTER TABLE news
-ADD FOREIGN KEY (publisher) REFERENCES users(id);
+    ADD FOREIGN KEY (publisher) REFERENCES users(id);
 
 ALTER TABLE eventstudent
-ADD FOREIGN KEY (listener_id) REFERENCES users(id);
+    ADD FOREIGN KEY (studentid) REFERENCES users(id);
 
 ALTER TABLE eventstudent
-ADD FOREIGN KEY (event_id) REFERENCES events(e_id);
+    ADD FOREIGN KEY (eventid) REFERENCES events(id);
 
 
 ALTER TABLE clubstudent
-ADD FOREIGN KEY (volunteers_id) REFERENCES users(id);
+    ADD FOREIGN KEY (studentid) REFERENCES users(id);
 
 ALTER TABLE clubstudent
-ADD FOREIGN KEY (club_id) REFERENCES clubs(c_id);
+    ADD FOREIGN KEY (clubid) REFERENCES clubs(id);
+
 
 
